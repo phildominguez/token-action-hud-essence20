ae97881b1a0fee8abfdcb264b4c4421aac8a6e19
Phil Dominguez <phild23@gmail.com>
2023-04-21 22:07:51 -0400

    Consts refactor

--- scripts/action-handler.js
+++ scripts/action-handler.js
@@ -1,31 +1,12 @@
 import { CoreActionHandler, CoreUtils } from './config.js'
 import { skillActions, supportedActors } from './constants.js'
 import {
-  INFO_ALT_MODES_ID,
-  INFO_ARMOR_ID,
-  INFO_BONDS_ID,
-  INFO_CLASS_FEATURES_ID,
-  INFO_CONTACTS_ID,
-  INFO_FEATURES_ID,
-  INFO_GEAR_ID,
-  INFO_HANG_UPS_ID,
-  INFO_ID,
-  INFO_ORIGINS_ID,
-  INFO_MAGIC_BAUBLES_ID,
-  INFO_MEGAFORM_TRAITS_ID,
-  INFO_PERKS_ID,
-  INFO_POWERS_ID,
-  INFO_SPELLS_ID,
-  INFO_THREAT_POWERS_ID,
-  INFO_WEAPONS_ID,
   INITIATIVE_ID,
-  POWERS_ID,
-  SKILLS_ID,
+  ITEMS,
   SKILLS_SMARTS_ID,
   SKILLS_SOCIAL_ID,
   SKILLS_SPEED_ID,
   SKILLS_STRENGTH_ID,
-  WEAPONS_ID,
 } from './defaults.js';
 
 export class ActionHandler extends CoreActionHandler {
@@ -69,61 +50,30 @@ export class ActionHandler extends CoreActionHandler {
   }
 
   _addWeaponsActions(actor) {
-    this._addActionHelper(this._getActionsForItemType('weapon', actor), WEAPONS_ID);
+    this._addActionHelper(
+      this._getActionsForItemType(ITEMS.weapons.type, actor),
+      ITEMS.weapons.id,
+    );
   }
 
   _addPowersActions(actor) {
-    this._addActionHelper(this._getActionsForItemType('power', actor), POWERS_ID);
+    this._addActionHelper(
+      this._getActionsForItemType(ITEMS.powers.type, actor),
+      ITEMS.powers.id,
+    );
   }
 
   _addInfoActions(actor) {
-    // Common items
-    this._addActionHelper(
-      this._getActionsForItemType('classFeature', actor, 'info'), INFO_CLASS_FEATURES_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('weapon', actor, 'info'), INFO_WEAPONS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('armor', actor, 'info'), INFO_ARMOR_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('contact', actor, 'info'), INFO_CONTACTS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('gear', actor, 'info'), INFO_GEAR_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('power', actor, 'info'), INFO_POWERS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('perk', actor, 'info'), INFO_PERKS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('origin', actor, 'info'), INFO_ORIGINS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('bond', actor, 'info'), INFO_BONDS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('hangUp', actor, 'info'), INFO_HANG_UPS_ID);
-
-    // Transformer items
-    this._addActionHelper(
-      this._getActionsForItemType('altMode', actor, 'info'), INFO_ALT_MODES_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('feature', actor, 'info'), INFO_FEATURES_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('megaformTrait', actor, 'info'), INFO_MEGAFORM_TRAITS_ID);
-
-    // Pony items
-    this._addActionHelper(
-      this._getActionsForItemType('spell', actor, 'info'), INFO_SPELLS_ID);
-    this._addActionHelper(
-      this._getActionsForItemType('magicBauble', actor, 'info'), INFO_MAGIC_BAUBLES_ID);
-
-    // Threat items
-    this._addActionHelper(
-      this._getActionsForItemType('threatPower', actor, 'info'), INFO_THREAT_POWERS_ID);
+    for (let [_, item] of Object.entries(ITEMS)) {
+      this._addActionHelper(
+        this._getActionsForItemType(item.type, actor, 'info'), item.infoId);
+    }
   }
 
   _getActionsForItemType(type, actor, actionId = 'item') {
     return actor.items.filter((i) => !!i && i.type == type)
       .map((i) => {
-        let encodedValue = [actionId, i.id].join(
-          this.delimiter
-        );
+        let encodedValue = [actionId, i.id].join(this.delimiter);
         return { name: i.name, encodedValue: encodedValue, id: i.id };
       });
   }
--- scripts/defaults.js
+++ scripts/defaults.js
@@ -1,24 +1,13 @@
-/**
- * Top-level Categories
- */
-
 export const INITIATIVE_NAME = "Initiative";
 export const INITIATIVE_ID = 'id-initiative';
 
 export const SKILLS_NAME = "Skills";
 export const SKILLS_ID = 'id-skills';
 
-export const WEAPONS_NAME = "Weapons";
-export const WEAPONS_ID = 'id-weapons';
-export const INFO_WEAPONS_ID = 'id-info-weapons';
-
-export const CLASS_FEATURES_NAME = "Class Features";
-export const INFO_CLASS_FEATURES_ID = 'id-info-class-features';
-
 export const INFO_NAME = "Info";
 export const INFO_ID = 'id-info';
 
-/* Subcategories - Skills */
+/* Skills */
 
 export const STRENGTH_NAME = "Strength";
 export const SKILLS_STRENGTH_ID = 'id-skills-strength';
@@ -32,139 +21,181 @@ export const SKILLS_SMARTS_ID = 'id-skills-smarts';
 export const SOCIAL_NAME = "Social";
 export const SKILLS_SOCIAL_ID = 'id-skills-social';
 
-/* Subcategories - Common Items */
+/* Subcategories - Items */
 
-export const ARMOR_NAME = "Armor";
-export const INFO_ARMOR_ID = 'id-info-armor';
-
-export const CONTACTS_NAME = "Contacts";
-export const INFO_CONTACTS_ID = 'id-info-contacts';
-
-export const GEAR_NAME = "Gear";
-export const INFO_GEAR_ID = 'id-info-gear';
-
-export const POWERS_NAME = "Powers";
-export const POWERS_ID = 'id-powers';
-export const INFO_POWERS_ID = 'id-info-powers';
-
-export const PERKS_NAME = "Perks";
-export const INFO_PERKS_ID = 'id-info-perks';
-
-export const ORIGINS_NAME = "Origins";
-export const INFO_ORIGINS_ID = 'id-info-origins';
-
-export const BONDS_NAME = "Bonds";
-export const INFO_BONDS_ID = 'id-info-bonds';
-
-export const HANG_UPS_NAME = "Hang-Ups";
-export const INFO_HANG_UPS_ID = 'id-info-hang-ups';
-
-/* Subcategories - Transformers Items */
-
-export const ALT_MODES_NAME = "Alt Modes";
-export const INFO_ALT_MODES_ID = 'id-info-alt-modes';
-
-/* Subcategories - Zord Items */
-
-export const FEATURES_NAME = "Features";
-export const INFO_FEATURES_ID = 'id-info-features';
-
-export const MEGAFORM_TRAITS_NAME = "Megaform Traits";
-export const INFO_MEGAFORM_TRAITS_ID = 'id-info-megaform-traits';
-
-/* Subcategories - Pony Items */
-
-export const SPELLS_NAME = "Spells";
-export const INFO_SPELLS_ID = 'id-info-spells';
-
-export const MAGIC_BAUBLES_NAME = "Magic Baubles";
-export const INFO_MAGIC_BAUBLES_ID = 'id-info-magic-baubles';
-
-/* Subcategories - NPC Items */
-
-export const THREAT_POWERS_NAME = "Threat Powers";
-export const INFO_THREAT_POWERS_ID = 'id-info-threat-powers';
+export const ITEMS = {
+  classFeatures: {
+    name: "Class Features",
+    infoId: 'id-info-class-features',
+    type: 'classFeature',
+    nestId: 'nest-id-info-class-features',
+  },
+  weapons: {
+    name: "Weapons",
+    infoId: 'id-info-weapons',
+    type: 'weapon',
+    nestId: 'nest-id-info-weapons',
+  },
+  armor: {
+    name: "Armor",
+    infoId: 'id-info-armor',
+    type: 'armor',
+    nestId: 'nest-id-info-armor',
+  },
+  contacts: {
+    name: "Contacts",
+    infoId: 'id-info-contacts',
+    type: 'contact',
+    nestId: 'nest-id-info-contacts',
+  },
+  gear: {
+    name: "Gear",
+    infoId: 'id-info-gear',
+    type: 'gear',
+    nestId: 'nest-id-info-gear',
+  },
+  powers: {
+    name: "Powers",
+    id: 'id-powers',
+    infoId: 'id-info-powers',
+    type: 'power',
+    nestId: 'nest-id-info-powers',
+  },
+  perks: {
+    name: "Perks",
+    infoId: 'id-info-perks',
+    type: 'perk',
+    nestId: 'nest-id-info-perks',
+  },
+  origins: {
+    name: "Origins",
+    infoId: 'id-info-origins',
+    type: 'origin',
+    nestId: 'nest-id-info-origins',
+  },
+  bonds: {
+    name: "Bonds",
+    infoId: 'id-info-bonds',
+    type: 'bond',
+    nestId: 'nest-id-info-bonds',
+  },
+  hangUps: {
+    name: "Hang-Ups",
+    infoId: 'id-info-hang-ups',
+    type: 'hangUp',
+    nestId: 'nest-id-info-hang-ups',
+  },
+  altModes: {
+    name: "Alt Modes",
+    infoId: 'id-info-alt-modes',
+    type: 'altMode',
+    nestId: 'nest-id-info-alt-modes',
+  },
+  features: {
+    name: "Features",
+    infoId: 'id-info-features',
+    type: 'feature',
+    nestId: 'nest-id-info-features',
+  },
+  megaformTraits: {
+    name: "Megaform Traits",
+    infoId: 'id-info-megaform-traits',
+    type: 'megaformTrait',
+    nestId: 'nest-id-info-megaform-traits',
+  },
+  spells: {
+    name: "Spells",
+    infoId: 'id-info-spells',
+    type: 'spell',
+    nestId: 'nest-id-info-spells',
+  },
+  magicBaubles: {
+    name: "Magic Baubles",
+    infoId: 'id-info-magic-baubles',
+    type: 'magicBauble',
+    nestId: 'nest-id-info-spells',
+  },
+  threatPowers: {
+    name: "Threat Powers",
+    infoId: 'id-info-threat-powers',
+    type: 'threatPower',
+    nestId: 'nest-id-info-threat-powers',
+  },
+};
 
 export const DEFAULTS = {
   categories: [ // The first row of buttons always visible on the HUD
     {
+      type: 'system',
       nestId: INITIATIVE_ID,
       id: INITIATIVE_ID,
       name: INITIATIVE_NAME,
-      type: 'system',
       subcategories: [ // The action groups visible when a Category is hovered/clicked
         {
+          type: 'system',
           nestId: 'nest-id-initiative',
           id: INITIATIVE_ID,
           name: "",
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
       ],
     },
     {
+      type: 'system',
       nestId: SKILLS_ID,
       id: SKILLS_ID,
       name: SKILLS_NAME,
-      type: 'system',
       subcategories: [
         {
+          type: 'system',
           nestId: 'nest-id-strength',
           id: SKILLS_STRENGTH_ID,
           name: STRENGTH_NAME,
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
         {
+          type: 'system',
           nestId: 'nest-id-strength',
           id: SKILLS_SPEED_ID,
           name: SPEED_NAME,
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
         {
+          type: 'system',
           nestId: 'nest-id-smarts',
           id: SKILLS_SMARTS_ID,
           name: SMARTS_NAME,
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
         {
+          type: 'system',
           nestId: 'nest-id-social',
           id: SKILLS_SOCIAL_ID,
           name: SOCIAL_NAME,
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
       ],
     },
     {
-      nestId: WEAPONS_ID,
-      id: WEAPONS_ID,
-      name: WEAPONS_NAME,
       type: 'system',
+      nestId: ITEMS.weapons.id,
+      id: ITEMS.weapons.id,
+      name: ITEMS.weapons.name,
       subcategories: [
         {
+          type: 'system',
           nestId: 'nest-id-weapons',
-          id: WEAPONS_ID,
+          id: ITEMS.weapons.id,
           name: "",
-          type: 'system',
-          hasDerivedSubcategories: false,
         },
       ],
     },
     {
-      nestId: POWERS_ID,
-      id: POWERS_ID,
-      name: POWERS_NAME,
       type: 'system',
+      nestId: ITEMS.powers.id,
+      id: ITEMS.powers.id,
+      name: ITEMS.powers.name,
       subcategories: [
         {
-          nestId: 'nest-id-powers',
-          id: POWERS_ID,
-          name: "",
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: 'nest-id-powers',
+          id: ITEMS.powers.id,
+          name: "",
         },
       ],
     },
@@ -172,119 +203,102 @@ export const DEFAULTS = {
       nestId: INFO_ID,
       id: INFO_ID,
       name: INFO_NAME,
-      type: 'system',
       subcategories: [
         {
-          nestId: 'nest-id-info-class-features',
-          id: INFO_CLASS_FEATURES_ID,
-          name: CLASS_FEATURES_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.classFeatures.nestId,
+          id: ITEMS.classFeatures.infoId,
+          name: ITEMS.classFeatures.name,
         },
         {
-          nestId: 'nest-id-info-weapons',
-          id: INFO_WEAPONS_ID,
-          name: WEAPONS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.weapons.nestId,
+          id: ITEMS.weapons.infoId,
+          name: ITEMS.weapons.name,
         },
         {
-          nestId: 'nest-id-info-weapons',
-          id: INFO_ARMOR_ID,
-          name: ARMOR_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.armor.nestId,
+          id: ITEMS.armor.infoId,
+          name: ITEMS.armor.name,
         },
         {
-          nestId: 'nest-id-info-contacts',
-          id: INFO_CONTACTS_ID,
-          name: CONTACTS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.contacts.nestId,
+          id: ITEMS.contacts.infoId,
+          name: ITEMS.contacts.name,
         },
         {
-          nestId: 'nest-id-info-gear',
-          id: INFO_GEAR_ID,
-          name: GEAR_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.gear.nestId,
+          id: ITEMS.gear.infoId,
+          name: ITEMS.gear.name,
         },
         {
-          nestId: 'nest-id-info-powers',
-          id: INFO_POWERS_ID,
-          name: POWERS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.powers.nestId,
+          id: ITEMS.powers.infoId,
+          name: ITEMS.powers.name,
         },
         {
-          nestId: 'nest-id-info-perks',
-          id: INFO_PERKS_ID,
-          name: PERKS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.perks.nestId,
+          id: ITEMS.perks.infoId,
+          name: ITEMS.perks.name,
         },
         {
-          nestId: 'nest-id-info-origins',
-          id: INFO_ORIGINS_ID,
-          name: ORIGINS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.origins.nestId,
+          id: ITEMS.origins.infoId,
+          name: ITEMS.origins.name,
         },
         {
-          nestId: 'nest-id-info-bonds',
-          id: INFO_BONDS_ID,
-          name: BONDS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.bonds.nestId,
+          id: ITEMS.bonds.infoId,
+          name: ITEMS.bonds.name,
         },
         {
-          nestId: 'nest-id-info-hang-ups',
-          id: INFO_HANG_UPS_ID,
-          name: HANG_UPS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.hangUps.nestId,
+          id: ITEMS.hangUps.infoId,
+          name: ITEMS.hangUps.name,
         },
         {
-          nestId: 'nest-id-info-alt-modes',
-          id: INFO_ALT_MODES_ID,
-          name: ALT_MODES_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.altModes.nestId,
+          id: ITEMS.altModes.infoId,
+          name: ITEMS.altModes.name,
         },
         {
-          nestId: 'nest-id-info-features',
-          id: INFO_FEATURES_ID,
-          name: FEATURES_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.features.nestId,
+          id: ITEMS.features.infoId,
+          name: ITEMS.features.name,
         },
         {
-          nestId: 'nest-id-info-megaform-traits',
-          id: INFO_MEGAFORM_TRAITS_ID,
-          name: MEGAFORM_TRAITS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.megaformTraits.nestId,
+          id: ITEMS.megaformTraits.infoId,
+          name: ITEMS.megaformTraits.name,
         },
         {
-          nestId: 'nest-id-info-spells',
-          id: INFO_SPELLS_ID,
-          name: SPELLS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.spells.nestId,
+          id: ITEMS.spells.infoId,
+          name: ITEMS.spells.name,
         },
         {
-          nestId: 'nest-id-info-spells',
-          id: INFO_MAGIC_BAUBLES_ID,
-          name: MAGIC_BAUBLES_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.magicBaubles.nestId,
+          id: ITEMS.magicBaubles.infoId,
+          name: ITEMS.magicBaubles.name,
         },
         {
-          nestId: 'nest-id-info-threat-powers',
-          id: INFO_THREAT_POWERS_ID,
-          name: THREAT_POWERS_NAME,
           type: 'system',
-          hasDerivedSubcategories: false,
+          nestId: ITEMS.threatPowers.nestId,
+          id: ITEMS.threatPowers.infoId,
+          name: ITEMS.threatPowers.name,
         },
       ],
     },
